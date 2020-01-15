7d5b02adf33395f5fb4fa3cfd0c7ad717adb2790
Michael Tesfaye <mtesfaye102@gmail.com>
2020-01-15 12:35:02 -0500

    Arrays for Each Team
    Created arrays from JSON that represent each teams's wins/losses/draws.

--- src/components/FetchStanding.jsx
+++ src/components/FetchStanding.jsx
@@ -2,6 +2,10 @@ import React, { Component } from 'react';
 import axios from 'axios';
 
 class FetchStanding extends Component {
+    state = {
+        posts: []
+    }
+
    componentDidMount () {
        axios({
                 url:'http://api.football-data.org/v2/competitions/2021/standings',
@@ -10,12 +14,46 @@ class FetchStanding extends Component {
                     'X-Auth-Token' : "6da71c4708c743a699e9c58c4505edfb"
                 }
             })
-            .then(response => {
+            .then(response => { 
+                this.setState({posts: response.data.standings[0].table});
                 console.log(response);
             })
    }
 
    render () {
+       let newData = this.state.posts;
+       console.log(newData);
+
+       let teamNames = [];
+       Object.keys(newData).forEach(function(key) {
+            teamNames.push(newData[key].team.name);
+       });
+       console.log(teamNames);
+
+       let teamCrests = [];
+       Object.keys(newData).forEach(function(key) {
+            teamCrests.push(newData[key].team.crestUrl);
+       });
+       console.log(teamCrests);
+
+       let teamWins = []
+       Object.keys(newData).forEach(function(key) {
+            teamWins.push(newData[key].won);
+       });
+       console.log(teamWins);
+
+       let teamDraws = []
+       Object.keys(newData).forEach(function(key) {
+            teamDraws.push(newData[key].draw);
+       });
+       console.log(teamDraws);
+
+       let teamLost = []
+       Object.keys(newData).forEach(function(key) {
+            teamLost.push(newData[key].lost);
+       });
+       console.log(teamLost);
+
        return (
            <div>
                <p>Hello</p>
